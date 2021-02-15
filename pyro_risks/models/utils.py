3e69565adf455c89bc3fa52c992956b115b34f87
Joshua Sant'Anna <45068597+dataJSA@users.noreply.github.com>
2021-02-15 19:41:01 +0100

    docs: Add docstrings to check_xy, check_x

--- pyro_risks/models/utils.py
+++ pyro_risks/models/utils.py
@@ -4,6 +4,18 @@ import numpy as np
 
 
 def check_xy(X: pd.DataFrame, y: pd.Series) -> Tuple[pd.DataFrame, pd.Series]:
+    """Validate inputs for transformers.
+
+    Args:
+        X: Training dataset features pd.DataFrame.
+        y: Training dataset target pd.Series.
+
+    Raises:
+        TypeError: Transformer methods expect pd.DataFrame and pd.Series as inputs.
+
+    Returns:
+        Copy of the inputs.
+    """
     if isinstance(X, pd.DataFrame) and isinstance(y, pd.Series):
         X = X.copy()
         y = y.copy()
@@ -14,6 +26,17 @@ def check_xy(X: pd.DataFrame, y: pd.Series) -> Tuple[pd.DataFrame, pd.Series]:
 
 
 def check_x(X: pd.DataFrame) -> pd.DataFrame:
+    """Validate inputs for tranformers.
+
+    Args:
+        X: Training dataset features pd.DataFrame.
+
+    Raises:
+        TypeError: Transformer methods expect pd.DataFrame as inputs.
+
+    Returns:
+        Copy of the inputs.
+    """
     if isinstance(X, pd.DataFrame):
         X = X.copy()
     else:
