0a404d2bc00dc3355ded9774748768e78474921c
Lucas-rbnt <luca.robinet@gmail.com>
2021-02-17 20:49:36 +0100

    Formatting with black test.py

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import numpy as np
 
+
 def printer():
-	return "This string is quite long, just a check to see if black is set up correctly and to do this way"
+    return "This string is quite long, just a check to see if black is set up correctly and to do this way"
